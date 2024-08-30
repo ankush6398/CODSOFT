4c96b187ced2696dbde99e8a876857a8ef6cd9ba
ankush6398 <140836699+ankush6398@users.noreply.github.com>
2024-08-30 08:43:46 +0530

    Add files via upload

---
+++ TASK-1/LEVEL-1 LANDING PAGE/script.js
@@ -0,0 +1,26 @@
+gsap.from("#img1",{
+    delay:0.4,
+    opacity:0,
+    duration:1.5,
+    y:20
+})
+gsap.from("#img2",{
+    delay:0.4,
+    opacity:0,
+    duration:1.5,
+    x:30
+})
+gsap.from("#img3",{
+    delay:0.4,
+    opacity:0,
+    duration:1.5,
+    y:10
+    
+})
+gsap.from("#main h1, #main  a",{
+    y:-30,
+    duration:1.5,
+  delay:0.5,
+    opacity:0,
+   Stagger:0.15
+})
